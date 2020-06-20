46e4de52af9546ea38129408b4a30058d533ddd3
mehedi rana <mdmehedirana@gmail.com>
2020-06-20 14:50:46 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+let allRead = true;
+
+let notifications = [
+    { message: 'Lorem', read: true },
+    { message: 'Ipsum', read: true },
+    { message: 'Dolor', read: true },
+    { message: 'Sit', read: true },
+    { message: 'Amet', read: true }
+];
