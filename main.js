b20ace139ec709408c597922ecc1ddcc940f9c68
mehedi rana <mdmehedirana@gmail.com>
2020-06-20 15:05:38 +0600

    debug

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ let notifications = [
     { message: 'Lorem', read: true },
     { message: 'Ipsum', read: true },
     { message: 'Dolor', read: true },
-    { message: 'Sit', read: true },
+    { message: 'Sit', read: false },
     { message: 'Amet', read: true }
 ];
 
